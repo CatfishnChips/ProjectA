b75af81d2bf45d0577b9cf7b79c799390315b7a2
CatfishnChips <denizpoyraz12@hotmail.com>
2023-02-01 18:43:01 +0300

    Update 2
    Finally decided on how the touch controls will be implemented in the Input Manager script. Added Event Manager to work together with the Input Manager to drive game events.

--- Assets/Scripts/TouchControls/InputManager.cs
+++ Assets/Scripts/TouchControls/InputManager.cs
@@ -1,9 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
+    #region Singleton
+
+    public static InputManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    #endregion
+
     [SerializeField] private LineRenderer _lineRenderer;
     private Vector3 pos;
     private bool isTouching;
@@ -12,11 +29,20 @@ public class InputManager : MonoBehaviour
     [SerializeField] private float _touchMoveTreshold = 0.1f;
     private Vector3 _lastFrameTouchPos;
 
+
+    private Vector2 _touchEndPosition;
+
+    private Touch _primaryTouch;
+    private Touch _touch1;
+    private Touch _touch2;
+
+
     // Update is called once per frame
     void Update()
     {
         if (Input.touchCount > 0) 
         {
+
             if (!isTouching) 
             {
                 DrawGesture();
@@ -24,19 +50,74 @@ public class InputManager : MonoBehaviour
                 isTouching = true;
             }
 
-            // Single Touch
-            // Touch touch = Input.GetTouch(0);
-            // Vector3 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
-            // touchPos.z = 0;
+            if (Input.touchCount == 1)
+            {
+                _primaryTouch = Input.GetTouch(0);
 
-            //pos = touchPos;
+                Vector3 touchScreenPosition = _primaryTouch.position;
+                touchScreenPosition.z = Camera.main.nearClipPlane;
+                Vector3 touchWorldPosition = Camera.main.ScreenToWorldPoint(_primaryTouch.position);
 
-            // Multiple Touches
-             foreach (Touch touch in Input.touches) 
-             {
-                Vector3 touchScreenPos = touch.position;
-                touchScreenPos.z = Camera.main.nearClipPlane;
-                if (touch.position.x < Screen.width / 2) 
+                pos = touchWorldPosition; // Temporary!
+
+                Vector2 touchMoveDirection = (_primaryTouch.rawPosition - _primaryTouch.position).normalized;
+                float touchMoveDistanceFromInitialPosition = Vector2.Distance(_primaryTouch.rawPosition, _primaryTouch.position);
+                Vector2 touchMoveDelta = _primaryTouch.deltaPosition;
+                float touchMoveSpeed = _primaryTouch.deltaPosition.magnitude / _primaryTouch.deltaTime;
+                //_primaryTouch.tapCount;
+
+                // Instead of using Touch's own states, making our own states might be better.
+                // Unity handles this automatically!
+                if (_primaryTouch.deltaPosition != Vector2.zero) 
+                {
+                    _primaryTouch.phase = TouchPhase.Stationary;
+                }
+                else
+                {
+                    _primaryTouch.phase = TouchPhase.Moved;
+                }
+
+                switch (_primaryTouch.phase) 
+                {
+                    case TouchPhase.Began:
+                    
+                    break;
+
+                    case TouchPhase.Moved:
+                    touchMoveSpeed = _primaryTouch.deltaPosition.magnitude / _primaryTouch.deltaTime;
+                    touchMoveDirection = (_primaryTouch.rawPosition - _primaryTouch.position).normalized;
+                    break;
+
+                    case TouchPhase.Ended:
+                    _touchEndPosition = _primaryTouch.position;
+
+                    EventManager.Instance.OnSwipe.Invoke(touchMoveDirection); // Example of an event invocation.
+                    break;
+                }
+
+                Debug.Log("Phase: " + _primaryTouch.phase + " StartPos: " + _primaryTouch.rawPosition + " Pos: " + _primaryTouch.position 
+                    + " Speed: " + touchMoveSpeed + " Dir: " + touchMoveDirection);
+
+                if (_primaryTouch.rawPosition.x < Screen.width / 2) 
+                {
+
+                }
+                else if (_primaryTouch.rawPosition.x > Screen.width / 2) 
+                {
+                    
+                }
+            }
+            else if (Input.touchCount == 2)
+            {
+
+            }
+       
+           // Multiple Touches
+                foreach (Touch touch in Input.touches) 
+                {
+                    Vector3 touchScreenPos = touch.position;
+                    touchScreenPos.z = Camera.main.nearClipPlane;
+                    if (touch.position.x < Screen.width / 2) 
                 {
                     Vector3 touchWorldPos = Camera.main.ScreenToWorldPoint(touchScreenPos);
                     //Debug.Log("ScreenPos: " + touchScreenPos + " WorldPos: " + touchWorldPos);
@@ -90,7 +171,6 @@ public class InputManager : MonoBehaviour
         // _lineRenderer.widthCurve = _lineCurve;
         // _lineRenderer.material = _lineMaterial;
         // _lineRenderer.positionCount = steps;
- 
     }
 
     private IEnumerator AddLinePoint() 
