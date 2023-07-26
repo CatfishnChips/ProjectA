24b86c5d57277d12634dacb6c9e6278db6d911f1
CatfishnChips <denizpoyraz12@hotmail.com>
2023-07-26 17:12:46 +0300

    HUD Improvements, Touch Trails Removed

--- Assets/Scripts/TouchControls/TouchIndicatorController.cs
+++ Assets/Scripts/TouchControls/TouchIndicatorController.cs
@@ -7,17 +7,9 @@ public class TouchIndicatorController : MonoBehaviour
 {
     [SerializeField] private Transform _touchIndicator1, _touchIndicator2;
     [SerializeField] private TextMeshProUGUI _touchText1, _touchText2;
-    [SerializeField] private Transform _trailObject1, _trailObject2;
-    private TrailRenderer _trail1, _trail2;
     private InputManager _inputManager = InputManager.Instance;
 
     private float _touchAHoldTime, _touchBHoldTime;
-
-    private void Awake() 
-    {
-        _trail1 = _trailObject1.GetComponent<TrailRenderer>();
-        _trail2 = _trailObject2.GetComponent<TrailRenderer>();
-    }
 
     private void OnDisable() 
     {
@@ -56,15 +48,12 @@ public class TouchIndicatorController : MonoBehaviour
         _touchIndicator1.transform.position = position;
         _touchText1.SetText("Began");
         _touchAHoldTime = 0;
-        _trailObject1.position = position;
-        _trail1.emitting = true;
     }
 
     private void DisableIndicator1(InputEventParams inputEventParams) 
     {
        _touchText1.SetText("Ended");
        _touchIndicator1.gameObject.SetActive(false);
-       _trail1.emitting = false;
     }
 
     private void MoveIndicator1(InputEventParams inputEventParams) 
@@ -73,7 +62,6 @@ public class TouchIndicatorController : MonoBehaviour
         _touchIndicator1.transform.position = position;
         _touchText1.SetText("Dragging");
         _touchAHoldTime = 0;
-        _trailObject1.position = position;
     }
 
     private void UpdateIndicator1(Vector2 screenPosition, Vector3 worldPosition) 
@@ -81,7 +69,6 @@ public class TouchIndicatorController : MonoBehaviour
         Vector3 position = new Vector3(worldPosition.x, worldPosition.y, -9f);
         _touchAHoldTime += Time.deltaTime;
         _touchText1.SetText("Hold - " + (int)_touchAHoldTime);
-        _trailObject1.position = position;
     }
 
     private void EnableIndicator2(Vector2 screenPosition, Vector3 worldPosition) 
@@ -91,15 +78,12 @@ public class TouchIndicatorController : MonoBehaviour
         _touchIndicator2.transform.position = position;
         _touchText2.SetText("Began");
         _touchBHoldTime = 0;
-        _trailObject2.position = position;
-        _trail2.emitting = true;
     }
 
     private void DisableIndicator2(InputEventParams inputEventParams) 
     {
         _touchText2.SetText("Ended");
         _touchIndicator2.gameObject.SetActive(false);
-        _trail2.emitting = false;
     }
 
     private void MoveIndicator2(InputEventParams inputEventParams) 
@@ -108,7 +92,6 @@ public class TouchIndicatorController : MonoBehaviour
         _touchIndicator2.transform.position = position;
         _touchText2.SetText("Dragging");
         _touchBHoldTime = 0;
-        _trailObject2.position = position;
     }
 
     private void UpdateIndicator2(Vector2 screenPosition, Vector3 worldPosition) 
@@ -116,6 +99,5 @@ public class TouchIndicatorController : MonoBehaviour
         Vector3 position = new Vector3(worldPosition.x, worldPosition.y, -9f);
         _touchBHoldTime += Time.deltaTime;
         _touchText2.SetText("Hold - " + (int)_touchBHoldTime);
-        _trailObject2.position = position;
     }
 }
